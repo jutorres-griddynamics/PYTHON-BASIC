69cc19f6941d0782efa56bb7a5d7dfaba838312b
jutorres-griddynamics <jutorres@griddynamics.com>
2022-06-24 14:52:32 -0500

    Part 6 Solving...

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -12,6 +12,8 @@ Sheets which are needed:
     Sheet's fields: Name, Code, Shares, Date Reported, % Out, Value.
     All fields except first two should be taken from Holders tab.
 
+    https://blog.jovian.ai/web-scraping-yahoo-finance-using-python-7c4612fab70c
+
 
 Example for the first sheet (you need to use same sheet format):
 ==================================== 5 stocks with most youngest CEOs ===================================
@@ -32,3 +34,50 @@ Links:
     - lxml docs: https://lxml.de/
 """
 
+import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+
+from selenium import webdriver
+import time
+def get_table_rows(driver):
+    """Get number of rows available on the page """
+    tablerows = len(driver.find_elements(By.XPATH, value='//*[@id="scr-res-table"]/div[1]/table/tbody/tr'))
+    return tablerows
+
+def get_table_header(driver):
+    """Return Table columns in list form """
+    header = driver.find_elements(By.TAG_NAME, value= 'th')
+    header_list = [item.text for index, item in enumerate(header) if index < 10]
+    return header_list
+
+def get_driver(url):
+    """Return web driver"""
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument('--log-level=3')
+
+    # Provide the path of chromedriver present on your system.
+    driver = webdriver.Chrome(executable_path="/Users/julitorresma/Downloads/chromedriver",
+                              chrome_options=options)
+    driver.get(url)
+
+    return driver
+
+driver = get_driver('https://finance.yahoo.com/cryptocurrencies')
+print(driver.title)
+header = driver.find_elements(By.TAG_NAME, value= 'th')
+print(header[0].text)
+print(header[2].text)
+
+txt=driver.find_element(By.XPATH, value='//*[@id="scr-res-table"]/div[1]/table/tbody/tr[1]').text
+
+print(get_table_rows(driver))
