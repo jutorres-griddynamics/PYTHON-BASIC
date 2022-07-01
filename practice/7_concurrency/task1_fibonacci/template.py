cba3a77e99b1df3729f62f6db3cf3b22e9d41bcf
jutorres-griddynamics <jutorres@griddynamics.com>
2022-07-01 12:30:01 -0500

    Part 7 Solved

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,10 +1,15 @@
 import os
 from random import randint
-
+import threading
+import time
+from threading import Thread
+from multiprocessing import Pool
+import csv
+import shutil
 
 OUTPUT_DIR = './output'
 RESULT_FILE = './output/result.csv'
-
+RESULTS = []
 
 def fib(n: int):
     """Calculate a value in the Fibonacci sequence by ordinal number"""
@@ -14,18 +19,39 @@ def fib(n: int):
         f0, f1 = f1, f0 + f1
     return f1
 
-
 def func1(array: list):
-    pass
+    return fib(array)
 
+def play_func1(listFib: list,callable:list):
+    for i in range(len(callable)):
+        file = 'output/'+str(callable[i])+'.txt'
+        with open(file, 'w+') as f:
+            f.write(str(listFib[i]))
 
-def func2(result_file: str):
-    pass
-
+def func2(result_file: str,callable:list):
+    f = open(result_file, 'w+', encoding='UTF8')
+    writer = csv.writer(f)
+    for i in callable:
+        with open(f'output/{i}.txt') as f:
+            contents = int(f.read())
+            writer.writerow([str(i),str(contents)])
 
 if __name__ == '__main__':
+
+    shutil.rmtree(OUTPUT_DIR)
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
+    starttime = time.time()
+    callable_list = list(set([randint(1000, 100000) for _ in range(1000)]))
+    endtime = time.time()
+    print(f"Time taken {endtime - starttime} seconds")
+    starttime = time.time()
+    pool = Pool()
+    list_fib =pool.map(func1, callable_list)
+    pool.close()
+    play_func1(list_fib,callable_list)
+    func2(RESULT_FILE,callable_list)
+    endtime = time.time()
+    print(f"Time taken {endtime - starttime} seconds")
 
-    func1(array=[randint(1000, 100000) for _ in range(1000)])
-    func2(result_file=RESULT_FILE)
+    #func2(result_file=RESULT_FILE)
--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,24 +1,42 @@
-API_KEY = "YOUR_KEY"
+import requests
+from multiprocessing import Pool
+import shutil
+import os
+import threading
+import time
+
+API_KEY = "hl9v3UacNJyy9IwWjlaiehDRS7Po0Z2zLwXexkBk"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
 OUTPUT_IMAGES = './output'
 
-
-def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
-
-
-def download_apod_images(metadata: list):
-    pass
+def get_apod_metadata(metadata: dict) -> list:
+    return requests.get(APOD_ENDPOINT,params=metadata).json()
 
 
+def download_apod_images(metadata):
+
+    if metadata['media_type'] == 'image':
+        file = open(f"output/{metadata['date']}.jpg", "wb")
+        response = requests.get(metadata['url'])
+        file.write(response.content)
+        file.close()
+    else:
+        pass
 def main():
-    metadata = get_apod_metadata(
-        start_date='2021-08-01',
-        end_date='2021-09-30',
-        api_key=API_KEY,
-    )
-    download_apod_images(metadata=metadata)
+    metadata = get_apod_metadata({'start_date':'2021-08-01','end_date':'2021-09-30','api_key':API_KEY})
+    #download_apod_images(metadata)
+    starttime = time.time()
+    pool = Pool()
+    pool.map(download_apod_images, metadata)
+    pool.close()
+    endtime = time.time()
+    print(f"Time taken {endtime - starttime} seconds")
+
 
 
 if __name__ == '__main__':
+    shutil.rmtree(OUTPUT_IMAGES)
+    if not os.path.exists(OUTPUT_IMAGES):
+        os.makedirs(OUTPUT_IMAGES)
+
     main()
